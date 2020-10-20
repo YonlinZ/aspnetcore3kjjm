312f3b451e46086154f3c76795b10ab1e200568b
yonlin zhu <zylin0721@foxmail.com>
2020-10-20 17:57:58 +0800

    合并提交0

---
+++ helloworld/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+
+namespace helloworld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Host.CreateDefaultBuilder()
+                .ConfigureWebHostDefaults(webHostBuilder => webHostBuilder
+                //.UseKestrel()
+                .ConfigureServices(services => services
+                    .AddRouting()
+                    .AddControllersWithViews())
+                .Configure(app => app
+                    .UseRouting()
+                    .UseEndpoints(endpoints => endpoints.MapControllers())
+                    /*.Run(
+                    context => context.Response.WriteAsync("Hello World!")
+                    )*/))
+                .Build()
+                .Run();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
